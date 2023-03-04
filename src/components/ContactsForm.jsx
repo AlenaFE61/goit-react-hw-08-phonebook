1e5684de42eea5032ab2bf2efa29179ff9c91e14
YukhymykAlena <o.yukhymyk@gmail.com>
2023-03-04 09:55:10 +0200

    jk

--- src/components/ContactsForm.jsx
+++ src/components/ContactsForm.jsx
@@ -36,7 +36,7 @@ export const ContacstForm = () => {
     enterContacts
       ? infoToast(`${name} or ${number} is already in contacts`)
       : addContact({ name, number });
-    !enterContacts && successToast('the contact is in the list');
+    !enterContacts && successToast('the contact is in the list  😃');
     setName('');
     setNumber('');
   };
